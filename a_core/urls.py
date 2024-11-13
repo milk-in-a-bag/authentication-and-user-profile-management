da4223879a153ae1772159b125e7bccd04170730
bryanmayodi <bryanmayodi@gmail.com>
2024-11-13 11:52:56 +0300

    finished profile edit functionality

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from a_home.views import *
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', home_view, name="home"),
     path('profile/', include('a_users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- a_users/urls.py
+++ a_users/urls.py
@@ -3,4 +3,5 @@ from a_users.views import *
 
 urlpatterns = [
     path('', profile_view, name="profile"),
+    path('edit/', profile_edit_view, name="profile-edit"),
 ]
