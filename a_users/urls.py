33a1e8e05a9f548884aadd339952c0b173a9a012
bryanmayodi <bryanmayodi@gmail.com>
2024-11-19 11:34:18 +0300

    settings page done

--- a_users/urls.py
+++ a_users/urls.py
@@ -4,4 +4,7 @@ from a_users.views import *
 urlpatterns = [
     path('', profile_view, name="profile"),
     path('edit/', profile_edit_view, name="profile-edit"),
+    path('onboarding/', profile_edit_view, name="profile-onboarding"),
+    path('settings/', profile_settings_view, name="profile-settings"),
+    path('emailchange/', profile_emailchange, name="profile-emailchange"),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .forms import *
 
 # Create your views here.
 
-def profile_view(request):
-    profile = request.user.profile
+def profile_view(request, username=None):
+
+    if username:
+        profile = get_object_or_404(User, username=username).profile
+    else:
+        try:
+            profile = request.user.profile
+        except:
+            return redirect('account_login')
     return render(request, 'a_users/profile.html', {'profile':profile})
 
 @login_required
@@ -17,5 +27,39 @@ def profile_edit_view(request):
         if form.is_valid():
             form.save()
             return redirect('profile')
+        
+    if request.path == reverse('profile-onboarding'):
+        onboarding = True
+    else:
+        onboarding = False
 
-    return render(request, 'a_users/profile_edit.html', {'form':form})
+    return render(request, 'a_users/profile_edit.html', {'form':form, 'onboarding':onboarding})
+
+@login_required
+def profile_settings_view(request):
+    return render(request, 'a_users/profile_settings.html')
+
+@login_required
+def profile_emailchange(request):
+
+    if request.htmx:
+        form = EmailForm(instance=request.user)
+        return render(request, 'partials/email_form.html', {'form': form})
+    
+    if request.method == 'POST':
+        form = EmailForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+
+            #check if email already exists
+
+            email = form.cleaned_data['email']
+
+            if User.objects.filter(email=email).exclude(id=request.user.id).exists():
+                messages.warning(request, f'{email} already exists!')
+                return redirect('profile-settings')
+
+            form.save()
+            return redirect('profile')
+    
+    return redirect('home')
